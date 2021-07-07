a51a8eb9a990bd13533ee5611d72a2ad3c74685a
RavshanDeveloperUz <rodilboyev@gmail.com>
2021-07-07 08:53:16 +0500

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,95 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_credit_card/credit_card_form.dart';
+import 'package:flutter_credit_card/credit_card_model.dart';
+import 'package:flutter_credit_card/credit_card_widget.dart';
+
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: DemoApp(),
+    );
+  }
+}
+
+class DemoApp extends StatefulWidget {
+  @override
+  _DemoAppState createState() => _DemoAppState();
+}
+
+class _DemoAppState extends State<DemoApp> {
+
+  String cardNumber = '';
+  String cardHolderName = '';
+  String cvvNumber = '';
+  String expiryDate = '';
+  bool showBackView = false;
+
+  void onCreditCardModel(CreditCardModel creditCardModel){
+    setState(() {
+      cardNumber = creditCardModel.cardNumber;
+      cardHolderName = creditCardModel.cardHolderName;
+      expiryDate = creditCardModel.expiryDate;
+      cvvNumber = creditCardModel.cvvCode;
+      showBackView = creditCardModel.isCvvFocused;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Credit Card UI'),
+        centerTitle: true,
+        backgroundColor: Colors.deepPurple,
+      ),
+      body: SafeArea(
+        child: Column(
+          children: [
+            SizedBox(height: 10,),
+            CreditCardWidget(
+              cardNumber: cardNumber,
+              expiryDate: expiryDate,
+              height: 210,
+              cardHolderName: cardHolderName,
+              cvvCode: cvvNumber,
+              showBackView: showBackView,
+              cardBgColor: Colors.blueGrey,
+              textStyle: TextStyle(
+                color: Colors.orange,
+                fontSize: 24.0,
+                fontWeight: FontWeight.bold,
+              ),
+              animationDuration: Duration(milliseconds: 1200),
+            ),
+            Expanded(
+              child: SingleChildScrollView(
+                child: CreditCardForm(
+                  onCreditCardModelChange: onCreditCardModel,
+                  cursorColor: Colors.red,
+                  themeColor: Colors.indigo,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
